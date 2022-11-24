2c0bf44dc642b06f2d768ac5559a27f964cadcb5
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2022-11-24 13:44:07 +0300

    Update index.html Update styles.css Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     nav_desktop.classList.remove("active");
 }))
+
+const popup = document.querySelector('.btn')
+const card = document.querySelector('.pr-btn')
+const show = document.querySelector(".popcontainer")
+const mob = document.querySelector('.overlay')
+popup.addEventListener("click",() => {
+    show.classList.toggle("active")
+} )
+
+card.addEventListener("click",() => {
+    show.classList.toggle("active")
+} )
+
+// card.addEventListener("click",() => {
+//     mob.classList.toggle("active")
+// } )
+const closebtn = document.querySelector(".closebtn");
+closebtn.addEventListener("click", () => {
+    show.classList.remove("active")
+})
