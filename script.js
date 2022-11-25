030441aba31fabcd67cccb4a146ae5fa0c87d672
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2022-11-25 16:47:17 +0300

    Fix linters Update script.js Update styles.css

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-const hamburger = document.querySelector(".hamburger");
-const nav_desktop = document.querySelector(".nav-mobile");
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    nav_desktop.classList.toggle("active");
-})
+const hamburger = document.querySelector('.hamburger');
+const nav_desktop = document.querySelector('.nav-mobile');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  nav_desktop.classList.toggle('active');
+});
 
-document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", ()=> {
-    hamburger.classList.remove("active");
-    nav_desktop.classList.remove("active");
-}))
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  nav_desktop.classList.remove('active');
+}));
 
-const popup = document.querySelector('.btn')
-const card = document.querySelector('.pr-btn')
-const show = document.querySelector(".popcontainer")
-const mob = document.querySelector('.overlay')
-popup.addEventListener("click",() => {
-    show.classList.toggle("active")
-} )
+const popup = document.querySelector('.btn');
+const card = document.querySelector('.pr-btn');
+const show = document.querySelector('.popcontainer');
+const mob = document.querySelector('.overlay');
+popup.addEventListener('click', () => {
+  show.classList.toggle('active');
+});
 
-card.addEventListener("click",() => {
-    show.classList.toggle("active")
-} )
+card.addEventListener('click', () => {
+  show.classList.toggle('active');
+});
 
 // card.addEventListener("click",() => {
 //     mob.classList.toggle("active")
-// } )
-const closebtn = document.querySelector(".closebtn");
-closebtn.addEventListener("click", () => {
-    show.classList.remove("active")
-})
+// })
 
-/*///////////Form validation\\\\\\\\\ */
+const closebtn = document.querySelector('.closebtn');
+closebtn.addEventListener('click', () => {
+  show.classList.remove('active');
+});
 
-document.getElementById("form").addEventListener("submit", (event) => {
-const email = document.getElementById("email").value;
-const emailError = document.getElementById("span");
-const form = document.getElementById("form");
-if (email === email.toLowerCase()) {
-    emailError.textContent = "Successfull";
+/*  Form validation     */
+
+document.getElementById('form').addEventListener('submit', (event) => {
+  const email = document.getElementById('email').value;
+  const emailError = document.getElementById('span');
+  const form = document.getElementById('form');
+  if (email === email.toLowerCase()) {
+    emailError.textContent = 'Successfull';
     form.submit();
-    } else {
+  } else {
     event.preventDefault();
-    emailError.textContent = "Please enter your email in lower case letters!";
-    }
+    emailError.textContent = 'Please enter your email in lower case letters!';
+  }
 });
